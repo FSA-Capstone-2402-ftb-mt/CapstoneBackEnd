335a803907bec59500c7f005a10070587daf5d7f
Alberto Martinez <avmrbeto98@gmail.com>
2024-08-15 18:24:55 -0700

    move fetch all users

--- models/user.js
+++ models/user.js
@@ -103,4 +103,13 @@ export const fetchUserByUsername = async (username) => {
   }
 };
 
-// 
+// Function to get all users
+export const getAllUsers = async () => {
+  try {
+    const { rows } = await client.query(`SELECT * FROM users`);
+    return rows;
+  } catch (error) {
+    console.error("Failed to get all users!", error);
+    throw error;
+  }
+};
