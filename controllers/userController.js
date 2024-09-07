68744f1be05ef7c50b59c2fffdbf2b887d870d1a
Alberto Martinez <avmrbeto98@gmail.com>
2024-09-07 07:58:23 -0700

    Changed response to login

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,7 +57,7 @@ export const loginUser = async (req, res) => {
         const token = generateToken(user);
         res.status(200).json({message: "Login successful", token, admin, username});
     } catch (error) {
-        res.status(500).json({message: "Failed to Log In"});
+        res.status(500).json({message: "Failed to Log In", error});
     }
 };
 
